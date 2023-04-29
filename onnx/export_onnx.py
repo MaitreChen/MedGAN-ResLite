84494f1430f572ef747fc0930353728df755934e
thebin <1974911747@qq.com>
2023-04-29 16:29:27 +0800

    Refine export_onnx.py

--- onnx/export_onnx.py
+++ onnx/export_onnx.py
@@ -1,17 +1,22 @@
 import argparse
+import os
 
 import torch
 
 from models.resnet import ResNet, BasicBlock
 
 
-def export_onnx(model, ckpt_path, output_dir):
+def export_onnx(ckpt_path, output_dir):
+    # build model and load checkpoints
+    model = ResNet(BasicBlock, [2, 2, 2, 2], num_classes=2).cuda()
     state_dict = torch.load(ckpt_path)
     model.load_state_dict(state_dict)
     model.eval()
 
-    # export onnx
+    # prepare input
     dummy_input = torch.randn(1, 1, 224, 224).cuda()
+
+    # export onnx
     torch.onnx.export(model, dummy_input, output_dir, input_names=['input'],
                       output_names=['output'], verbose=True,
                       opset_version=11)
@@ -22,18 +27,14 @@ def export_onnx(model, ckpt_path, output_dir):
 if __name__ == "__main__":
     # Define cmd arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default='')
-    parser.add_argument('--ckpt-path', type=str,
-                        default='../checkpoints/cls/exp_fake_scratch_pretrained_test/pneumonia_best.pth')
-    parser.add_argument('--output-path', type=str, default='./original_resnet.onnx')
-
+    parser.add_argument('--ckpt-path', type=str, required=True, default='./pretrained/resnet18-sam.pth')
+    parser.add_argument('--output-path', type=str, required=True, default='./pretrained/resnet18-sam.onnx')
     args = parser.parse_args()
 
-    # Check input argument
-    ckpt_path = args.ckpt_path
-    output_path = args.output_path
+    # Check input arguments
+    if not os.path.exists(args.ckpt_path):
+        print(f'Cannot find checkpoint path: {args.ckpt_path}')
+        exit()
 
-    # Build model
-    model = ResNet(BasicBlock, [2, 2, 2, 2], num_classes=2).cuda()
-
-    export_onnx(model, ckpt_path, output_path)
+    # Run to export
+    export_onnx(args.ckpt_path, args.output_path)
