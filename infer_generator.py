7d98a7dfe3c4f559b6b22835c7c8894e3ed670c5
thebin <1974911747@qq.com>
2023-05-01 23:40:30 +0800

    Add three generation mode

--- infer_generator.py
+++ infer_generator.py
@@ -1,33 +1,7 @@
 """
 Here is a script for sn-dcgan to generate image visualizations.
 At the same time, you can use this script to generate minority class samples to train a classifier.
-
-Here are some tips if you want to generate minority class samples:
-First, you can set mode like this distinguish between different situations
----------------------------------------------------------------
-        set mode = 0 if batch_size == 1349 else 1
----------------------------------------------------------------
-Where:
-        1349 means generate single images for classifier and evaluation;
-        64 means generate a sprite image for visualization;
-
-Secondly, you can create folder to save single images or sprite
----------------------------------------------------------------
-            save_path = f'outs/exp_{lab_name}/'
-            create_folder(save_path + 'single/')
-            create_folder(save_path + 'sprite/')
----------------------------------------------------------------
-
-Then, you can run this script!
----------------------------------------------------------------
-    # Run to save
-    infer(ckpt_path, batch_size, z_dim, image_size, save_path, save_mode=mode)
----------------------------------------------------------------
-
-Finally, you can get what you want in direct;
 """
-
-from matplotlib import pyplot as plt
 from time import time
 import cv2 as cv
 import argparse
@@ -36,7 +10,6 @@ import os
 import torchvision
 import torch
 
-from utils.dcgan_dataset import get_dataloader_dcgan
 from utils.image_utils import decode_to_numpy
 from utils.common_utils import create_folder
 
@@ -47,21 +20,26 @@ from utils.common_utils import create_folder
 from models.sndcgan import Generator
 
 
-def infer(ckpt_path, batch_size, z_dim, image_size, save_path, save_mode):
+def infer(ckpt_path, batch_size, z_dim, image_size, device, save_path, save_mode, save_dir='figures/generator_torch/'):
     """
-    :param ckpt_path: checkpoints of dcgan
-    :param batch_size: #
-    :param z_dim: default 128
-    :param image_size: default 128
-    :param save_path: #
-    :param save_mode: 0:save single images; 1:save mini-batch images(Sprite chart)
+    Args:
+        ckpt_path: checkpoints of dcgan
+        batch_size: #
+        z_dim: default 128
+        image_size: default 128
+        device: #
+        save_path: #
+        save_mode: 0: generate images for training; 1:save mini-batch images(Sprite map); -1: generate single image
+        save_dir: Inference result saving dir
     """
+    create_folder(save_dir)
+
     out_channels = 1
 
     # Create random tensors
     fixed_z = torch.randn(batch_size, z_dim)
     fixed_z = fixed_z.view(fixed_z.size(0), fixed_z.size(1), 1, 1)
-    print(f"fixed_z.shape={fixed_z.shape}")
+    # print(f"fixed_z.shape={fixed_z.shape}")
 
     # Define model and load checkpoint
     G = Generator(z_dim=z_dim, image_size=image_size, out_channels=out_channels)
@@ -73,10 +51,17 @@ def infer(ckpt_path, batch_size, z_dim, image_size, save_path, save_mode):
     start_time = time()
     fake_images = G(fixed_z.to(device))
     end_time = time()
-    print(f"Infer time: {end_time - start_time:.6f} ms")
+    # print(f"Infer time: {end_time - start_time:.6f} ms")
 
-    # save images
-    if save_mode == 0:
+    # Generate & Save images
+    if save_mode == -1:
+        print("Generate a single image...")
+        fake_image_numpy, fake_image = decode_to_numpy(fake_images[0])
+        fake_image = fake_image.transpose(1, 2, 0)
+        cv.imwrite(save_dir + 'fake.png', fake_image)
+    elif save_mode == 0:
+        print("Generate a Sprite map...")
+        create_folder(save_path + 'single/')
         idx = 0
         for i in range(batch_size):
             fake_image_numpy, fake_image = decode_to_numpy(fake_images[i][0])
@@ -84,58 +69,35 @@ def infer(ckpt_path, batch_size, z_dim, image_size, save_path, save_mode):
             cv.imwrite(path, fake_image)
             idx += 1
     elif save_mode == 1:
+        print("Generate a batch of images...")
+        create_folder(save_path + 'sprite/')
         save_fake_path = f"{save_path}/sprite/fake.png"
         torchvision.utils.save_image(fake_images, save_fake_path)
-    else:
-        pass
-
-    # Show fake images
-    fig = plt.figure(figsize=(8, 8))
-    fig.suptitle("fake images", fontsize=34)
-    for i in range(batch_size):
-        plt.subplot(5, 5, i + 1)
-        fake_image_numpy, fake_image = decode_to_numpy(fake_images[i][0])
-        plt.imshow(fake_image_numpy, 'gray')
-        plt.axis('off')
-
-    plt.show()
-
-    """
-    # Create Dataset AND save real images
-    train_dataloader = get_dataloader_dcgan(batch_size)
-    batch_iterator = iter(train_dataloader)  # convert to iteration
-    real_images = next(batch_iterator)  # get the first element
-    torchvision.utils.save_image(real_images, 'real.png')
-    
-    # show real images
-    fig = plt.figure(figsize=(8, 8))
-    fig.suptitle("real images", fontsize=34)
-    for i in range(batch_size):
-        plt.subplot(5, 5, i + 1)
-        plt.imshow(decode_to_numpy(real_images[i][0])[0], 'gray')
-        plt.axis('off')
-    plt.show()
-    """
 
 
 if __name__ == "__main__":
     # Define cmd arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch-size', type=str, default=25,
-                        help='batch size for inference. (default: 25)')
-    parser.add_argument('--image-size', type=int, default=64, help='image size (default: 128)')
-    parser.add_argument('--z-dim', type=int, default=128, help='z-dim of generator (default: 128)')
-    parser.add_argument('--ckpt-path', type=str, default='pretrained/sn-dcgan.pth',
+    parser.add_argument('--ckpt-path', type=str, required=True, default='pretrained/sn-dcgan.pth',
                         help='sn-dcgan checkpoints path for inference')
-    parser.add_argument('--use_gpu', action='store_true', default=True, help='turn on flag to use GPU')
+    parser.add_argument('--batch-size', type=int, required=True, default=1,
+                        help='batch size for inference.')
+    parser.add_argument('--image-size', type=int, default=64,
+                        help='image size (default: 128)')
+    parser.add_argument('--z-dim', type=int, default=128,
+                        help='z-dim of generator (default: 128)')
+    parser.add_argument('--device', type=str, default='cpu',
+                        help='inference device. [cuda | cpu]')
+    parser.add_argument('--save-path', type=str, default='outs/',
+                        help=' the path to save the generation fake images (single & sprite) (default: outs/)')
+    parser.add_argument('--mode', type=int, default=-1, required=True,
+                        help='generate images for [single:-1 | images:0 | sprite:1] (default: -1)')
     args = parser.parse_args()
 
     # Check input argument
-    batch_size = args.batch_size
-    image_size = args.image_size
-    z_dim = args.z_dim
-    ckpt_path = args.ckpt_path
-    device = 'cuda' if args.use_gpu else 'cpu'
+    if not os.path.exists(args.ckpt_path):
+        print(f'Cannot find the dcgan checkpoints: {args.ckpt_path}')
+        exit()
 
-    # Run to see
-    infer(ckpt_path, batch_size, z_dim, image_size, save_path=-1, save_mode=-1)
+    # Run to generate fake images
+    infer(args.ckpt_path, args.batch_size, args.z_dim, args.image_size, args.device, args.save_path, args.mode)
